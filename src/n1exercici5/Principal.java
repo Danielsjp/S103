7597cd155ec044012d42ea7fc72d2d7ca20530f6
Dani Sán <40555221+Danielsjp@users.noreply.github.com>
2022-03-29 11:45:27 +0200

    .
    .

--- src/n1exercici5/Principal.java
+++ src/n1exercici5/Principal.java
@@ -9,7 +9,9 @@ public class Principal {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		ArrayList<String> Lista1 = new ArrayList<String>();
-		ArrayList<String> ListaInvertida=new ArrayList<String>();
+		ArrayList<String> ListaInvertida = new ArrayList<String>();
+		ArrayList<String> ListaInvertida2 = new ArrayList<String>();
+
 		Lista1.add("Geeks");
 		Lista1.add("for");
 		Lista1.add("Geeks");
@@ -23,7 +25,7 @@ public class Principal {
 		System.out.println("\nThe iterator values(ORIGINAL)" + " of Arraylist are: ");
 		while (iter.hasNext()) {
 			System.out.print(iter.next().toString() + " ");
-			
+
 		}
 		Collections.reverse(Lista1);
 		for (String item : Lista1) {
@@ -33,26 +35,19 @@ public class Principal {
 		System.out.println("\nMETHOD1#The iterator values(inverted)" + " of Arraylist are: ");
 		while (iter2.hasNext()) {
 			System.out.print(iter2.next().toString() + " ");
-			
+
 		}
-		// Arraylist for storing reversed elements
-        ArrayList<String> ListaInvertida2 = new ArrayList<String>();
-        for (int i = Lista1.size() - 1; i >= 0; i--) {
- 
-            // Append the elements in reverse order
-        	ListaInvertida2.add(Lista1.get(i));
-        }
-        Iterator<String> iter3 = ListaInvertida2.iterator();
-        System.out.println("\nMETHOD2#The iterator values(reversed)" + " of Arraylist are: ");
+
+		for (int i = Lista1.size() - 1; i >= 0; i--) {
+
+			ListaInvertida2.add(Lista1.get(i));
+		}
+		Iterator<String> iter3 = ListaInvertida2.iterator();
+		System.out.println("\nMETHOD2#The iterator values(reversed)" + " of Arraylist are: ");
 		while (iter3.hasNext()) {
 			System.out.print(iter3.next().toString() + " ");
-			
+
 		}
-        // Return the reversed arraylist
- 
-    }
+
 	}
-		
-
-
-
+}
